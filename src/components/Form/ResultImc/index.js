456ea9c04b2ce3a7bce316c03c5f4b6836fb628a
batista7557 <gabriel.bat2001@gmail.com>
2021-11-27 08:46:07 -0300

    Uso da API Share

--- src/components/Form/ResultImc/index.js
+++ src/components/Form/ResultImc/index.js
@@ -1,10 +1,28 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity, Share, Touchable } from 'react-native'
 import styles from './style'
 
 export default function ResultImc(props){
+
+    const onShare = async () => {
+        const result = await Share.share({
+            message:'Meu imc hoje é: ' +props.resultImc,
+        })
+    }
+
     return(
         <View style={styles.resultImc}>
+            <View style={styles.boxShareButton}>
+                {props.resultImc != null ?
+                <TouchableOpacity 
+                onPress={onShare}
+                style={styles.shared}>
+                    <Text style={styles.sharedText}>Share</Text>
+                </TouchableOpacity>
+                :
+                <View/>
+                }
+            </View>
             <Text style={styles.information}>{props.mensageResultImc}</Text>
             <Text style={styles.numberImc}>{props.resultImc}</Text>
         </View>
--- src/components/Form/ResultImc/style.js
+++ src/components/Form/ResultImc/style.js
@@ -18,6 +18,22 @@ const styles = StyleSheet.create({
         fontSize:18,
         color:'#ff0043',
         fontWeight:'bold',
+    },
+    boxShareButton:{
+        width:'100%',
+        alignItems:'center',
+        marginBottom:10,
+    },
+    shared:{
+        backgroundColor:'#1877f2',
+        borderRadius:50,
+        paddingBottom:5,
+        paddingTop:5,
+    },
+    sharedText:{
+        color:'#fff',
+        fontWeight:'bold',
+        paddingHorizontal:30,
     }
 });
 
