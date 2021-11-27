59b4834e848a3c886d2fb964f5afe92a1d797543
batista7557 <gabriel.bat2001@gmail.com>
2021-11-27 12:35:38 -0300

    Ajustes

--- src/components/Form/ResultImc/style.js
+++ src/components/Form/ResultImc/style.js
@@ -1,10 +1,17 @@
 import { StyleSheet } from 'react-native'
 
 const styles = StyleSheet.create({
+    contextImc:{
+        flex:1,
+        marginTop:20,
+        paddingTop:15,
+        alignItems:'center',
+        width:'100%',
+    },
     resultImc:{
         flex:1,
-        marginTop:15,
-        paddingTop:60,
+        marginTop:0,
+        paddingTop:10,
         borderRadius:50,
         alignItems:'center',
         width:'100%',
@@ -22,6 +29,7 @@ const styles = StyleSheet.create({
     boxShareButton:{
         width:'100%',
         alignItems:'center',
+        marginTop:0,
         marginBottom:10,
     },
     shared:{
--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { View, Text, TextInput, TouchableOpacity, Vibration } from 'react-native'
+import { View, Text, TextInput, TouchableOpacity, Vibration, Pressable, Keyboard } from 'react-native'
 import ResultImc from './ResultImc/'
 import styles from './style'
 
@@ -13,7 +13,8 @@ export default function Form(){
     const [errorMessage, setErrorMessage] = useState(null)
 
     function imcCalculator(){
-        return setImc((weight/(height * height)).toFixed(2))
+        let heightFormat = height.replace(',','.')
+        return setImc((weight/(heightFormat * heightFormat)).toFixed(2))
     }
 
     function verificationImc(){
@@ -46,7 +47,7 @@ export default function Form(){
     }
 
     return(
-        <View style={styles.formContext}>
+        <Pressable onPress={Keyboard.dismiss} style={styles.formContext}>
             <View style={styles.form}>
                 <Text style={styles.formLabel}>Altura</Text>
                 <TextInput
@@ -73,6 +74,6 @@ export default function Form(){
             </TouchableOpacity>
             </View>
             <ResultImc mensageResultImc={mensageImc} resultImc={imc}/>
-        </View>
+        </Pressable>
     )
 }
